713d82af1c5e487e80e90bfcd88e06c40e7e30bf
LINGESH R <lingesh.r.official@gmail.com>
2025-05-10 11:12:38 +0530

    Update main.py

--- server_/main.py
+++ server_/main.py
@@ -4,12 +4,15 @@ import numpy as np
 from io import BytesIO
 from PIL import Image
 
+from ultralytics import YOLO
+import cv2
 import tensorflow as tf
 import cv2
+import time 
 
 app=FastAPI()
 
-MODEL=tf.keras.models.load_model(r'C:\Users\ADMIN\Desktop\ML_and_DL\Waste Classification\model\1.keras')
+MODEL=tf.keras.models.load_model(r'1.keras')
 CLASS_NAMEs=['NON-RECYCLABLE','RECYCLABLE']
 # class_names_=
 
@@ -43,31 +46,68 @@ async def predict(
 
 @app.get('/predict_camera')
 async def predict_camera():
-    # web_cam=cv2.VideoCapture(0)
-    # while True:
-    #     ret,frame=web_cam.read()
-    #     cv2.imshow('frame',frame)
-    #     if cv2.waitKey(1) & 0xFF==ord('c'):
-    #         cv2.imwrite(r'Computer_Vision\try\k1.jpg',frame)
-    #     if cv2.waitKey(1) & 0xFF==ord('q'):
-    #         break
+        
+    model = YOLO("yolov5s.pt")
+    # Open the webcam (camera index 0)
+    cap = cv2.VideoCapture(0)
+    if not cap.isOpened():
+        return {"error": "Could not access webcam"}
+    
+    time.sleep(5)
 
-    # web_cam.release()
+    ret, frame = cap.read()
+    cap.release()
+    
+
+    if not ret:
+        return {"error": "Failed to capture image from webcam"}
+
+    # Run YOLOv5
+    results = model(frame)
+    result = results[0]
+    # cv2.imshow("Object Detection", results[0].plot())
+    # time.sleep(5)
     # cv2.destroyAllWindows()
 
-    file='Computer_Vision\try\k1.jpg'
-    # image= read_file_as_image(cv2.imread(file))
-    # img_batch=np.expand_dims(cv2.imread(file),0)
-    prediction=MODEL.predict(cv2.imread(file))
-    # print(prediction)
-    index_=np.argmax(prediction[0])
-    predicted_class=CLASS_NAMEs[index_]
-    confidence=np.max(prediction[0])
+    max_area = 0
+    largest_box = None
+
+    for box in result.boxes:
+        x1, y1, x2, y2 = map(int, box.xyxy[0])
+        area = (x2 - x1) * (y2 - y1)
+        if area > max_area:
+            max_area = area
+            largest_box = (x1, y1, x2, y2)
+
+    if largest_box is None:
+        return {"error": "No object detected"}
+
+    x1, y1, x2, y2 = largest_box
+    
+    cropped_img = result.orig_img[y1:y2, x1:x2]
+    cv2.imwrite(r'lar_object.jpg',cropped_img)
+
+
+    # Resize and normalize for classifier
+    resized_img = cv2.resize(cropped_img, (256, 256))
+    # normalized = resized_img / 255.0
+    cv2.imwrite(r'largest_object.jpg',resized_img)
+    input_img = np.expand_dims(resized_img, axis=0)
+
+
+    # cv2.imshow('Processed image',resized_img)
+    # cv2.waitKey(5000)
+
+    prediction = MODEL.predict(input_img)
+    index = np.argmax(prediction[0])
+    confidence = float(np.max(prediction[0]))
+    predicted_class = CLASS_NAMEs[index]
 
     return {
-        'class':predicted_class,
-        'confidence':float(confidence)
+        "class": predicted_class,
+        "confidence": confidence
     }
 
+    
 if __name__=='__main__':
     uvicorn.run(app,host='localhost',port=8000)
